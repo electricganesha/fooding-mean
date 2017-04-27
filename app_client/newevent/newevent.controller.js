af01285dba5b322fc69b1506c8a24b2c5dc9fdcc
Ricardo Pinto <ricardoadspinto@gmail.com>
2017-04-27 18:44:18 +0100

    Página criar evento 60%

--- app_client/newevent/newevent.controller.js
+++ app_client/newevent/newevent.controller.js
@@ -4,8 +4,8 @@
     .module('fooding')
     .controller('newEventCtrl', newEventCtrl);
 
-    newEventCtrl.$inject = ['$scope', 'categoriesData'];
-    function newEventCtrl ($scope, categoriesData) {
+    newEventCtrl.$inject = ['$scope', 'categoriesData', 'Upload'];
+    function newEventCtrl ($scope, categoriesData, Upload) {
       console.log('New Event controller is running');
 
       $scope.selected = [];
@@ -61,6 +61,60 @@
         },function (error){
           $scope.message = "Sorry, something's gone wrong, please try again later";
         });
+
+
+      /*// upload on file select or drop
+      $scope.upload = function (file) {
+          console.log(file);
+          Upload.upload({
+              url: '/img/foonding_covers/',
+              data: {file: file}
+          }).then(function (resp) {
+              console.log('Success ' + resp.config.data.file.name + 'uploaded. Response: ' + resp.data);
+          }, function (resp) {
+              console.log('Error status: ' + resp.status);
+          }, function (evt) {
+              var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+              console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
+          });
+      };*/
+
+      $scope.$watch('files', function () {
+          console.log($scope.files);
+          $scope.upload($scope.files);
+      });
+      $scope.$watch('file', function () {
+          if ($scope.file != null) {
+              $scope.files = [$scope.file]; 
+          }
+      });
+      $scope.log = '';
+
+      $scope.upload = function (files) {
+          if (files && files.length) {
+              for (var i = 0; i < files.length; i++) {
+                var file = files[i];
+                if (!file.$error) {
+                  Upload.upload({
+                      url: '/img/foonding_covers/',
+                      data: {
+                        file: file  
+                      }
+                  }).then(function (resp) {
+                      console.log(files);
+                      console.log(document.getElementById("drop-box"));
+                      document.getElementById("drop-box").style.backgroundImage = "url('" + file + "')";
+                  }, null, function (evt) {
+                      var progressPercentage = parseInt(100.0 *
+                          evt.loaded / evt.total);
+                      $scope.log = 'progress: ' + progressPercentage + 
+                        '% ' + evt.config.data.file.name + '\n' + 
+                        $scope.log;
+                  });
+                }
+              }
+          }
+      };
 
 
     }
