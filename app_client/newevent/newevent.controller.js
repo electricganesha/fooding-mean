7d4aed1ab672d9462b4455ceeb94709258f881c7
Ricardo Pinto <ricardoadspinto@gmail.com>
2017-04-28 19:41:08 +0100

    Página de criar evento 70%

--- app_client/newevent/newevent.controller.js
+++ app_client/newevent/newevent.controller.js
@@ -8,6 +8,11 @@
     function newEventCtrl ($scope, categoriesData, Upload) {
       console.log('New Event controller is running');
 
+      $scope.onTimeSet = function (newDate, oldDate) {
+          console.log(newDate);
+          console.log(oldDate);
+      }
+
       $scope.selected = [];
 
       $scope.slideIn = "slideOut";
@@ -62,25 +67,7 @@
           $scope.message = "Sorry, something's gone wrong, please try again later";
         });
 
-
-      /*// upload on file select or drop
-      $scope.upload = function (file) {
-          console.log(file);
-          Upload.upload({
-              url: '/img/foonding_covers/',
-              data: {file: file}
-          }).then(function (resp) {
-              console.log('Success ' + resp.config.data.file.name + 'uploaded. Response: ' + resp.data);
-          }, function (resp) {
-              console.log('Error status: ' + resp.status);
-          }, function (evt) {
-              var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
-              console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
-          });
-      };*/
-
       $scope.$watch('files', function () {
-          console.log($scope.files);
           $scope.upload($scope.files);
       });
       $scope.$watch('file', function () {
@@ -115,6 +102,96 @@
               }
           }
       };
+
+
+
+
+      $scope.today = function() {
+        $scope.dt = new Date();
+      };
+      $scope.today();
+
+      $scope.clear = function() {
+        $scope.dt = null;
+      };
+
+      $scope.inlineOptions = {
+        customClass: getDayClass,
+        minDate: new Date(),
+        showWeeks: true
+      };
+
+      $scope.dateOptions = {
+        dateDisabled: disabled,
+        formatYear: 'yy',
+        maxDate: new Date(2020, 5, 22),
+        minDate: new Date(),
+        startingDay: 1,
+        showWeeks: false
+      };
+
+      // Disable weekend selection
+      function disabled(data) {
+        var date = data.date,
+          mode = data.mode;
+        return mode === 'day' && (date.getDay() === 0 || date.getDay() === 6);
+      }
+
+      $scope.toggleMin = function() {
+        $scope.inlineOptions.minDate = $scope.inlineOptions.minDate ? null : new Date();
+        $scope.dateOptions.minDate = $scope.inlineOptions.minDate;
+      };
+
+      $scope.toggleMin();
+
+      $scope.open1 = function() {
+        $scope.popup1.opened = true;
+      };
+
+      $scope.setDate = function(year, month, day) {
+        $scope.dt = new Date(year, month, day);
+      };
+
+      $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+      $scope.format = $scope.formats[0];
+      $scope.altInputFormats = ['M!/d!/yyyy'];
+
+      $scope.popup1 = {
+        opened: false
+      };
+
+      var tomorrow = new Date();
+      tomorrow.setDate(tomorrow.getDate() + 1);
+      var afterTomorrow = new Date();
+      afterTomorrow.setDate(tomorrow.getDate() + 1);
+      $scope.events = [
+        {
+          date: tomorrow,
+          status: 'full'
+        },
+        {
+          date: afterTomorrow,
+          status: 'partially'
+        }
+      ];
+
+      function getDayClass(data) {
+        var date = data.date,
+          mode = data.mode;
+        if (mode === 'day') {
+          var dayToCheck = new Date(date).setHours(0,0,0,0);
+
+          for (var i = 0; i < $scope.events.length; i++) {
+            var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
+
+            if (dayToCheck === currentDay) {
+              return $scope.events[i].status;
+            }
+          }
+        }
+
+        return '';
+      }
 
 
     }
